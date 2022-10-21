72bc06b92fcdcd3245dbac556c00155e95a79869
Björn Norgren <bjorn.norgren@fripost.org>
2022-10-21 23:33:58 +0200

    Ls end

--- blogapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "blogapp": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
